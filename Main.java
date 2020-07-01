cae2f3888b7a89fa5acb2ad40b63e0f01672e64d
Francis <francis.tholley@scranton.edu>
2020-07-01 10:27:57 -0400

    graph

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        var graph = new Graph();
+        graph.addNode("A");
+        graph.addNode("B");
+        graph.addNode("C");
+        graph.addEdge("A", "B");
+        graph.addEdge("A", "C");
+        graph.addEdge("B", "C");
+        graph.removeNode("A");
+        graph.print();
+    }
+}
